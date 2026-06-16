7c699a3d8eff165136227839e22987a87297cba7
aleksskay2 <aleksskay2@yandex.ru>
2026-06-16 22:08:37 +0300

    give files

--- backend/base/urls.py
+++ backend/base/urls.py
@@ -14,23 +14,47 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+# from django.contrib import admin
+# from django.urls import path, include
+# from django.conf import settings
+# from django.conf.urls.static import static
+# from store.views import index
+# from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
+
+# urlpatterns = [
+#       path('',index, name='index'),  # об
+#     path('admin/', admin.site.urls),
+#     path('api/', include('store.urls')),
+#     path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+#     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+
+#     ]
+    
+
+# if settings.DEBUG:
+#     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+
+
+
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, re_path
 from django.conf import settings
-from django.conf.urls.static import static
+from django.views.static import serve
 from store.views import index
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
 urlpatterns = [
-      path('',index, name='index'),  # об
+    path('', index, name='index'),  # об
     path('admin/', admin.site.urls),
     path('api/', include('store.urls')),
     path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+]
 
-    ]
-    
-
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-
+# 🔥 ПРИНУДИТЕЛЬНАЯ РАЗДАЧА ФАЙЛОВ В ПРОДАКШЕНЕ
+# Заменили if settings.DEBUG на постоянную раздачу, чтобы Render отдавал картинки
+urlpatterns += [
+    re_path(r'^media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT}),
+    re_path(r'^static/(?P<path>.*)$', serve, {'document_root': settings.STATIC_ROOT}),
+]
