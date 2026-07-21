a303a3f7bd1b9e21fc19a3af33cdeae29bf48c5e
aleksskay2 <aleksskay2@yandex.ru>
2026-07-21 11:43:28 +0300

    i did bigger count message to first loading

--- backend/store/pagination.py
+++ backend/store/pagination.py
@@ -7,7 +7,7 @@ from rest_framework.response import Response
 from rest_framework.exceptions import NotFound
 
 class RegionChatPagination(PageNumberPagination):
-    page_size = 20
+    page_size = 50
     page_size_query_param = "page_size"
     max_page_size = 100
 
--- backend/store/views.py
+++ backend/store/views.py
@@ -1801,7 +1801,7 @@ def get_file_type(file):
 from rest_framework.pagination import PageNumberPagination
 
 class GroupMessagePagination(PageNumberPagination):
-    page_size = 20
+    page_size = 50
     page_size_query_param = "page_size"
     max_page_size = 50
 
